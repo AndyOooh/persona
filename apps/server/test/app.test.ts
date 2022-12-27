8cbe8b37186178375dc9df3c9a57e8f3ff86e897
AndyOoh <andy.oeee@gmail.com>
2022-12-28 04:16:50 +0700

    add question/:id test, passing

--- apps/server/test/app.test.ts
+++ apps/server/test/app.test.ts
@@ -7,8 +7,19 @@ describe('GET /questions', () => {
   });
   test('should return a non-empty array', async () => {
     const response  = await supertest(app).get('/questions');
-    console.log('🚀  file: app.test.ts:10  response', response)
     expect(response.body).toBeInstanceOf(Array);
-    expect(response.body.length).toBeGreaterThan(3);
+    expect(response.body.length).toBeGreaterThan(0);
+  });
+});
+
+describe('GET /questions/:id', () => {
+  test('should return 200 OK', () => {
+    return supertest(app).get('/questions/1').expect(200);
+  });
+  test('should return an object with the correct properties', async () => {
+    const response  = await supertest(app).get('/questions/1');
+    console.log('rsposnse', response.body)
+    expect(response.body).toBeInstanceOf(Object);
+    expect(response.body).toHaveProperty('id');
   });
 });

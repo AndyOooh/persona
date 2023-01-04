c3a97f96b08c25accba06f7935dafd266cf88794
AndyOoh <andy.oeee@gmail.com>
2023-01-04 19:23:47 +0700

    restructure apps folder

--- apps/client/app/(root)/layout.tsx
+++ apps/client/app/(root)/layout.tsx
@@ -1,8 +1,8 @@
 import { Brygada_1918 } from '@next/font/google';
 
-import Header from './components/Header';
-import './styles/globals.scss';
-import hero from '../public/hero/yeshi-kangrang-iuqxv7kFj64-unsplash.jpg';
+import Header from '../components/Header';
+import '../styles/globals.scss';
+import hero from '../../public/hero/yeshi-kangrang-iuqxv7kFj64-unsplash.jpg';
 
 const brygada = Brygada_1918({ subsets: ['latin'] });
 
